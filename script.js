84fc2371fe760901b2e9b7a56e5050aca9dbcf6d
Azrielle <azriellepelayo@gmail.com>
2020-06-20 00:26:11 +0800

    Created a method to make 9 div elements inside gameContainer Applied CSS to .gameContainer

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let gameContainer = document.querySelector(".gameContainer");
 
+
+const Gameboard = (() =>{
+    let gameboard = ["x", "o", "x", "o"];
+
+    return{
+        createGrid: ()=>{
+        let gameContainer = document.querySelector(".gameContainer");
+            for (let i = 0; i < 9; i++){
+                let grid = document.createElement("div");
+                grid.className = i;
+                gameContainer.appendChild(grid);
+            }
+        }
+    }
+})();
+
+Gameboard.createGrid();
