ee986321c3fccafd23326fdb6403c73285548807
Azrielle <azriellepelayo@gmail.com>
2020-06-22 18:00:18 +0800

    Fixed the issue of playerDisplay not changing names even when there is user input on the form Changed the display values of playerDisplay, gameContainer, and formContainer in style.css Added displayForm function

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const Gameboard = (() =>{
     let playerDisplay = document.querySelector(".playerDisplay");
     let resultContainer = document.querySelector(".resultContainer");
     let playAgainButton = document.querySelector(".playAgain");
+    let startButton = document.querySelector(".start")
+    let formContainer = document.querySelector(".formContainer");
+    let playerOne = document.querySelector("#playerOne");
+    let playerTwo = document.querySelector("#playerTwo");
   
     function renderArray(){
         for (let i =0; i <9; i++){
@@ -23,12 +27,35 @@ const Gameboard = (() =>{
     const player = (name, move, winner) => {
         return {name, move, winner}
     }
-    
-    const player1 = player("Player 1", "X", false);
-    const player2 = player("Player 2", "O", false);
-    let turn = null;
    
-    function startGame(){
+ 
+
+    let turn = null;
+
+    function displayForm(){
+        gameContainer.style.display = "none";
+        playerDisplay.style.display = "none";
+        formContainer.style.display = "block";
+        playerOne.value = "";
+        playerTwo.value = "";
+
+        pressToStart();
+    }
+
+    function pressToStart(){
+        startButton.addEventListener("click", event=>{
+            if(event.target == event.currentTarget){
+                formContainer.style.display = "none";
+                playerDisplay.style.display = "flex";
+                gameContainer.style.display = "grid";
+                const player1 = player(playerOne.value|| "Player 1", "X", false);
+                const player2 = player(playerTwo.value || "Player 2", "O", false);
+                startGame(player1, player2);
+            }
+        })
+    }
+   
+    function startGame(player1, player2){
         turn = true;
         let player = null;
         displayPlayerTurn(player1);
@@ -58,7 +85,7 @@ const Gameboard = (() =>{
          event.stopPropagation();
         })
     }
-    return {startGame};
+ 
 
     function playAnotherGame(){
         playAgainButton.addEventListener("click", () =>{
@@ -71,16 +98,16 @@ const Gameboard = (() =>{
             playerDisplay.style.backgroundColor 
             resultContainer.style.visibility = "hidden";
             turn = true;
-            startGame();
+            displayForm();
         })
     }
 
     function displayPlayerTurn(player){
         playerDisplay.textContent = `${player.name}'s turn`;
-        if(player == player1){
+        if(player.move == "X"){
             playerDisplay.style.backgroundColor = "#ED9797"
         }
-        else if (player == player2){
+        else if (player.move == "O"){
             playerDisplay.style.backgroundColor = "#65C5DA"
         }
     }
@@ -142,6 +169,7 @@ const Gameboard = (() =>{
         playerDisplay.style.backgroundColor = "#8D8D8D";
         playAnotherGame()
     }
+    return {displayForm};
 })();
 
-Gameboard.startGame();
+Gameboard.displayForm();
